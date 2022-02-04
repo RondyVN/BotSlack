698134244903b4ab768a1964360d94b5d1716e01
RondyVN <rondelman15@gmail.com>
2022-02-04 08:56:32 +0200

    add modal window order

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import {SLACK_APP_TOKEN, SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET} from "./constants";
+
 const {App} = require('@slack/bolt');
 import {typeOfDishes, dishes} from "./objectsDishes";
 import {getOptionObject} from "./getOptionObject";
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: SLACK_BOT_TOKEN,
+    signingSecret: SLACK_SIGNING_SECRET,
     socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
+    appToken: SLACK_APP_TOKEN,
     // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
     // you still need to listen on some port!
     port: process.env.PORT || 3000
@@ -38,46 +40,140 @@ app.message('hello', async ({say}) => {
     });
 });
 
-app.action('static_select-action', async ({body, ack, say}) => {
+app.action('static_select-action', async ({body, ack, client}) => {
     // Acknowledge the action
     await ack();
     const typeOfDish = body.actions[0].selected_option.value;
 
-    await say(`<@${body.user.id}> picked the ${typeOfDish}`);
+    // await say(`<@${body.user.id}> picked the ${typeOfDish}`);
 
-    const optionDishes = getOptionObject(dishes[typeOfDish])
-    await say({
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Pick type of eat"
-                },
-                "accessory": {
-                    "type": "static_select",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Select type",
-                        "emoji": true
+    //const optionDishes = getOptionObject(dishes[typeOfDish])
+
+    await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: {
+            type: 'modal',
+            // View identifier
+            callback_id: 'view_1',
+            title: {
+                type: 'plain_text',
+                text: 'Order dishes'
+            },
+            submit: {
+                type: 'plain_text',
+                text: 'Submit',
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Choose a dish to order"
                     },
-                    "options": optionDishes,
-                    "action_id": "order"
+                    "accessory": {
+                        "type": "multi_static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select an item",
+                            "emoji": true
+                        },
+                        "options": getOptionObject(dishes[typeOfDish]),
+                        "action_id": "order"
+                    }
                 }
-            }
-        ],
-    })
+            ]
+        }
+    });
+
+    //logger.info(result)
+
+    // await say({
+    //     blocks: [
+    //         {
+    //             "type": "section",
+    //             "text": {
+    //                 "type": "mrkdwn",
+    //                 "text": "Pick type of eat"
+    //             },
+    //             "accessory": {
+    //                 "type": "static_select",
+    //                 "placeholder": {
+    //                     "type": "plain_text",
+    //                     "text": "Select type",
+    //                     "emoji": true
+    //                 },
+    //                 "options": optionDishes,
+    //                 "action_id": "order"
+    //             }
+    //         }
+    //     ],
+    // })
 });
 
-app.action('order', async ({body, ack, say}) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> order the ${body.actions[0].selected_option.value}`)
+app.view('view_1', async ({ack, body, view, client, logger}) => {
+    await ack;
+    const keysOrder = Object.keys(view.state.values)
+    console.log(view.state.values[keysOrder]['order']['selected_options'])
+    const orders = view.state.values[keysOrder]['order']['selected_options']
+
+    try {
+        await client.chat.postMessage({
+            channel: 'C0319SBGSJ2',
+            text: `<@${body.user.id}> order the ${orders.map(order => order.value + ' ')}`
+        })
+    }
+    catch (error){
+        logger.error(error)
+    }
 });
+
+// app.action('order', async ({ack, client, body}) => {
+//     await ack;
+//     await client.chat.postMessage({
+//         channel: 'C0319SBGSJ2',
+//         text: `<@${body.user.id}> order ${body.actions[0].selected_option.value}`
+//     })
+// });
+
+// app.action('order', async ({body, ack, client}) => {
+//     // Acknowledge the action
+//     await ack();
+//     const typeOfDish = body.actions[0].selected_option;
+//     const result = await client.views.update({
+//         view_id: body.view.id,
+//         // Pass the current hash to avoid race conditions
+//         hash: body.view.hash,
+//         view: {
+//             type: 'modal',
+//             // View identifier
+//             callback_id: 'view_1',
+//             title: {
+//                 type: 'plain_text',
+//                 text: 'Modal title'
+//             },
+//             "blocks": [
+//                 {
+//                     "type": "section",
+//                     "text": {
+//                         "type": "mrkdwn",
+//                         "text": "This is a section block with an accessory image."
+//                     },
+//                     "accessory": {
+//                         "type": "image",
+//                         "image_url": "https://assets.misteram.com.ua/misteram-public/b1a88a19a4d7bd3b41703ce1ba99802e-826x0.png",
+//                         "alt_text": "cute cat"
+//                     }
+//                 },
+//             ]
+//         }
+//     })
+//     //await say(`<@${body.user.id}> order the ${body.actions[0].selected_option.value}`)
+// });
 
 (async () => {
     // Start your app
     await app.start();
-
     console.log('⚡️ Bolt app is running!');
 })();
--- src/objectsDishes.js
+++ src/objectsDishes.js
@@ -1,42 +1,49 @@
 export const typeOfDishes = {
-    "meat": "meat",
-    "drink": "drink",
-    "Vegetables": "Vegetables",
+    "Мясо і стейки": "Мясо",
+    "Напої": "Апої",
+    "Овочні": "Овочні",
 }
 
 export const dishes = {
-    "meat": {
-        "Pork": {
+    "Мясо і стейки": {
+        "Чікен бургер з картоплею фрі": {
             "Price": 120,
-            "Weight": "150 грам"
+            "Weight": "150 грам",
+            "Image": "https://assets.misteram.com.ua/misteram-public/06dce4046da0840a2b3ca7419c9ec99f-826x0.png",
         },
-        "chicken": {
+        "Клаб бургер з котлетою": {
             "Price": 90,
-            "Weight": "100 грам"
+            "Weight": "100 грам",
+            "Image": "https://assets.misteram.com.ua/misteram-public/eb39c6bfe5d522d02126e42d0d74e2ef-826x0.png"
         }
     },
-    "drink": {
+    "Напої": {
         "CocaCola": {
             "Price": 25,
-            "Weight": "1.5 Літра"
+            "Weight": "1.5 Літра",
+            "Image": "https://assets.misteram.com.ua/misteram-public/488519a74dcf1eb50c8a9d73f8acf0e6-826x0.png"
         },
         "Coffee": {
             "Price": 40,
-            "Weight": "0.5 Літра"
+            "Weight": "0.5 Літра",
+            "Image": "https://assets.misteram.com.ua/misteram-public/2fff276b780c7ce7f72dfb09efe9504d-826x0.png"
         }
     },
-    "Vegetables": {
+    "Овочні": {
         "Potato": {
             "Price": 10,
-            "Weight": "100 грам"
+            "Weight": "100 грам",
+            "Image": "https://assets.misteram.com.ua/misteram-public/04ca562e249a306a4fbe0047fdb29577-826x0.png"
         },
         "Cucumber": {
             "Price": 10,
-            "Weight": "100 грам"
+            "Weight": "100 грам",
+            "Image": "https://assets.misteram.com.ua/misteram-public/4a5baaa9670d709b6bb87bbb9c624c32-826x0.png"
         },
         "Tomato": {
             "Price": 10,
-            "Weight": "100 грам"
+            "Weight": "100 грам",
+            "Image": "https://assets.misteram.com.ua/misteram-public/eb39c6bfe5d522d02126e42d0d74e2ef-826x0.png"
         },
     }
 }
